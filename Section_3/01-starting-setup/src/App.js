7bb41453492cea34635e90d90e263741a50379d5
Kihun SONG <kihunbuzz@naver.com>
2023-01-03 17:50:32 +0900

    Section 3-47

--- Section_3/01-starting-setup/src/App.js
+++ Section_3/01-starting-setup/src/App.js
@@ -1,6 +1,6 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -26,32 +26,9 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
-
-// const expenseDate = new Date(2021, 2, 28);
-// const expenseTitle = "Car Insurance";
-// const expenseAmount = 294.67;
