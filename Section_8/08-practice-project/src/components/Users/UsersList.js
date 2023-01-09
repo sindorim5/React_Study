284bdde2ec0a9e30715e4d6f7aff616f0a91a872
Kihun SONG <kihunbuzz@naver.com>
2023-01-09 23:39:28 +0900

    Section 8-108

---
+++ Section_8/08-practice-project/src/components/Users/UsersList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Card from "../UI/Card";
+import classes from "./UsersList.module.css";
+
+const UsersList = (props) => {
+  return (
+    <Card className={classes.users}>
+      <ul>
+        {/* users가 undefined이기 때문에 정의해주어야 함 */}
+        {props.users.map((user) => (
+          <li>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+};
+
+export default UsersList;
