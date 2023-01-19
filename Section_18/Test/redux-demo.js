bbcb39519da17740ee2f103e613211442e17aee1
sindorim5 <kihunbuzz@naver.com>
2023-01-20 00:26:54 +0900

    Section 18-254

---
+++ Section_18/Test/redux-demo.js
@@ -0,0 +1,32 @@
+const redux = require("redux");
+
+// state의 초기 값이 필요하다
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
+
+const store = redux.createStore(counterReducer);
+
+console.log(store.getState());
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+// dispatch로 counterSubscriber 실행
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
---
+++ Section_18/src/store/index.js
@@ -0,0 +1,62 @@
+import { createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+// export const INCREMENT = "increment";
+
+const initialState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+const counterReducer = (state = initialState, action) => {
+  // if (action.type === INCREMENT) {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+      showCounter: state.showCounter,
+    };
+  }
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
+    };
+  }
+  return state;
+};
+
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
+
+export default store;
